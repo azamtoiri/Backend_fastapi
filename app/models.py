ddc9f324688ae2baabee4fa359bb537b64cf94e7
azamtoiri <azamtoiri@gmail.com>
2022-10-04 04:18:15 +0300

    Created User model

--- app/models.py
+++ app/models.py
@@ -13,3 +13,12 @@ class Post(Base):
     content: str = Column(String, nullable=False)
     published: bool = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: int = Column(Integer, primary_key=True, nullable=False)
+    email: str = Column(String, nullable=False, unique=True)
+    password: str = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
