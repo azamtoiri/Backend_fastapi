081678c734a5ef115f5346b0a627730d6bab3559
azamtoiri <azamtoiri@gmail.com>
2022-10-13 21:44:37 +0300

    MODIFIED: get query for post

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,9 +1,10 @@
 from typing import List, Optional
 
 from fastapi import APIRouter, Response, status, HTTPException, Depends
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
-from app.db.database import engine, get_db
+from app.db.database import get_db
 from app import schemas, oauth2, models
 
 
@@ -13,11 +14,14 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=List[schemas.Post])
+# @router.get("/")
+@router.get("/", response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
               limit: int = 10, skip: int = 0, search: Optional[str] = ""):
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    return posts
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(
+        models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    return post
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
@@ -36,9 +40,10 @@ def get_latest_post(db: Session = Depends(get_db), current_user: int = Depends(o
     return last_post
 
 
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post_id(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} not fount")
     return post
